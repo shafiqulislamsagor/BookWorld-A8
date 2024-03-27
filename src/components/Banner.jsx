d84701a9d9fe79edde7c2ca4a3fe3ef09b272759
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-03-27 08:14:41 +0600

    banner style done

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,8 +6,8 @@ const Banner = () => {
             <div className="hero-content py-16 w-[80%] mx-auto flex-col lg:flex-row-reverse">
                 <img src="https://i.ibb.co/6N3HT30/book9.png" className="max-w-sm rounded-lg shadow-2xl" />
                 <div>
-                    <h1 className="text-5xl font-bold mb-11">Books to freshen up your bookshelf</h1>
-                    <Link to={'/bookList'} className="btn btn-success">View The List</Link>
+                    <h1 className="text-5xl displayed font-bold leading-[74px] mb-11">Books to freshen up your bookshelf</h1>
+                    <Link to={'/bookList'} className="btn bg-[#23BE0A] work text-base text-white">View The List</Link>
                 </div>
             </div>
         </div>
