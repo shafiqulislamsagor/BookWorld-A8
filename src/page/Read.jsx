67815329c3f98326d58f0c6cb317d0405d31a482
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-03-28 00:04:25 +0600

    some issue

--- src/page/Read.jsx
+++ src/page/Read.jsx
@@ -1,8 +1,10 @@
 import ReadCard from "../components/ReadCard";
 import { getStore } from "../components/store";
+import { ActionClick } from './Sort';
 
 const Read = () => {
-    let readCard = getStore()
+    let readCard = ActionClick() || getStore()
+    // console.log(readCard);
     return (
         <div className="mb-7 mt-3">
             {
